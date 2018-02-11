ed28e0878a5bc5fb43315a2eecfda47b973031da
Kevin <36340428+Team4939Robotics@users.noreply.github.com>
2018-02-11 14:41:14 -0500

    2/11/2018 2:41PM
    edited timeouts on autos

--- src/org/usfirst/frc/team4939/robot/commands/auto/LeftSwitch.java
+++ src/org/usfirst/frc/team4939/robot/commands/auto/LeftSwitch.java
@@ -24,7 +24,7 @@ public class LeftSwitch extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
-    	addSequential(new DriveCommand(168, 0.5, 0, 1));
-    	addSequential(new TurnCommand(90, 0.5, 1));
+    	addSequential(new DriveCommand(168, 0.5, 0, 2.5));
+    	addSequential(new TurnCommand(90, 0.5, 2.5));
     }
 }
