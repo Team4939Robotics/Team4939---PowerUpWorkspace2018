a4886bd00d184d5f058ae94c8f705ac464533636
Kevin <36340428+Team4939Robotics@users.noreply.github.com>
2018-02-11 14:04:41 -0500

    2/11/2018 2:04PM
    idk man

--- src/org/usfirst/frc/team4939/robot/commands/auto/RightSwitch.java
+++ src/org/usfirst/frc/team4939/robot/commands/auto/RightSwitch.java
@@ -24,5 +24,7 @@ public class RightSwitch extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	addSequential(new DriveCommand(168, 0.5, 0, 1));
+    	addSequential(new TurnCommand(-90, 0.5, 1));
     }
 }
