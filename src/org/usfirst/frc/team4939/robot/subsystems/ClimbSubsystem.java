be5e53cce8177d1fe8c399ee7fafebb66aa8ff5d
Kevin <36340428+Team4939Robotics@users.noreply.github.com>
2018-02-18 10:08:42 -0500

    2/18/2018 10:08 AM
    commented out climber locking, added getPressureSwitchValue() for smartdashboard

--- src/org/usfirst/frc/team4939/robot/subsystems/ClimbSubsystem.java
+++ src/org/usfirst/frc/team4939/robot/subsystems/ClimbSubsystem.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team4939.robot.subsystems;
 import org.usfirst.frc.team4939.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -13,24 +13,24 @@ public class ClimbSubsystem extends Subsystem {
 	public static Spark climbMotor1 = new Spark(RobotMap.climbMotor1);
 	public static Spark climbMotor2 = new Spark(RobotMap.climbMotor2);
 	
-	public Solenoid lockMech = new Solenoid(RobotMap.lockPiston);
-	public boolean climberLocked = false;
+	//public DoubleSolenoid lockMech = new DoubleSolenoid(RobotMap.LeftLockPiston, RobotMap.RightLockPiston);
+	//public boolean climberLocked = false;
 	
 	public void useClimber(double speed) {
 		climbMotor1.set(speed);
 		climbMotor2.set(speed);
 	}
-	
+	/*
 	public void lockClimber() {
-		this.lockMech.set(true);
+		this.lockMech.set(DoubleSolenoid.Value.kForward);
 		this.climberLocked = true;
 	}
 	
 	public void unlockClimber() {
-		this.lockMech.set(false);
+		this.lockMech.set(DoubleSolenoid.Value.kReverse);
 		this.climberLocked = false;
 	}
-	
+	*/
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
